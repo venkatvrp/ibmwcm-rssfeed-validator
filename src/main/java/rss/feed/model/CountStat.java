eb3a0ce99b363f001cdf8e66e252eb7bad7042b0
Venkatesh Varatharajaperumal <venkatesh.v.perumal@accenture.com>
2018-09-12 11:32:18 +0800

    refactored and made logic changes

---
+++ src/main/java/rss/feed/model/CountStat.java
@@ -0,0 +1,47 @@
+package rss.feed.model;
+
+public class CountStat {
+	
+	int imgTtlCnt = 0;
+	int imgPassCnt = 0;
+	int imgFailCnt = 0;
+	/**
+	 * @return the imgTtlCnt
+	 */
+	public int getImgTtlCnt() {
+		return imgTtlCnt;
+	}
+	/**
+	 * @param imgTtlCnt the imgTtlCnt to set
+	 */
+	public void setImgTtlCnt(int imgTtlCnt) {
+		this.imgTtlCnt = imgTtlCnt;
+	}
+	/**
+	 * @return the imgPassCnt
+	 */
+	public int getImgPassCnt() {
+		return imgPassCnt;
+	}
+	/**
+	 * @param imgPassCnt the imgPassCnt to set
+	 */
+	public void setImgPassCnt(int imgPassCnt) {
+		this.imgPassCnt = imgPassCnt;
+	}
+	/**
+	 * @return the imgFailCnt
+	 */
+	public int getImgFailCnt() {
+		return imgFailCnt;
+	}
+	/**
+	 * @param imgFailCnt the imgFailCnt to set
+	 */
+	public void setImgFailCnt(int imgFailCnt) {
+		this.imgFailCnt = imgFailCnt;
+	}
+	
+	
+
+}
--- src/main/java/rss/feed/validator/RssFeedValidateTool.java
+++ src/main/java/rss/feed/validator/RssFeedValidateTool.java
@@ -1,3 +1,5 @@
+package rss.feed.validator;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -23,10 +25,12 @@ import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 
-public class RssFeedValidateTool {
+import rss.feed.model.CountStat;
 
-	final static Logger logger = LogManager.getLogger(RssFeedValidateTool.class);
-	final static ResourceBundle resourceBundle = ResourceBundle.getBundle("rssfeedvalidator");
+public class RssFeedValidateTool {
+	
+	static final ResourceBundle resourceBundle = ResourceBundle.getBundle("rssfeedvalidator");
+	static final Logger logger = LogManager.getLogger(RssFeedValidateTool.class);
 
 	public static void main(String[] args) {
 		RssFeedValidateTool rssFeedValidator = new RssFeedValidateTool();
@@ -45,13 +49,18 @@ public class RssFeedValidateTool {
 		}
 	}
 
+	/**
+	 * Validates the URL of the image present in the RSS Feed xml
+	 * @param rssFeedUrl
+	 * @return boolean
+	 */
 	@SuppressWarnings("unchecked")
 	private boolean validateImageURL(String rssFeedUrl) {
 		try {
 			URL feedUrl;
 			int imgTtlCnt = 0;
-			int imgPassCnt = 0;
 			int imgFailCnt = 0;
+			int imgPassCnt = 0;
 			feedUrl = new URL(rssFeedUrl);
 			SyndFeedInput input = new SyndFeedInput();
 			SyndFeed feed = input.build(new XmlReader(feedUrl));
@@ -60,48 +69,79 @@ public class RssFeedValidateTool {
 				List<Element> foreignElementList = (List<Element>) entry.getForeignMarkup();
 				for (Element element : foreignElementList) {
 					if (element.getAttributeValue("name") != null
-							&& element.getAttributeValue("name").startsWith("article-image")) {
-						for (Element childElem : (List<Element>) element.getChildren()) {
-							if (childElem != null && childElem.getContentSize() > 0
-									&& !childElem.getText().equals("image")) {
-								String url = childElem.getContent(0).getValue();
-								URL obj = new URL(url);
-								HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-								con.setRequestMethod("GET");
-								int responseCode = con.getResponseCode();
-								imgTtlCnt++;
-								if (responseCode == 404) {
-									logger.debug(entry.getTitle() + " content url invalid:: " + url);
-									imgFailCnt++;
-								} else {
-									imgPassCnt++;
-								}
-							}
-						}
+							&& (element.getAttributeValue("name").startsWith("article-image") ||
+									element.getAttributeValue("name").equals("regular-article-thumbnail-image") || 
+									element.getAttributeValue("name").contains("gallery-image"))) {
+						CountStat countStat = connectToURL(element,entry);
+						imgTtlCnt = imgTtlCnt + countStat.getImgTtlCnt();
+						imgFailCnt = imgFailCnt + countStat.getImgFailCnt();
+						imgPassCnt = imgPassCnt + countStat.getImgPassCnt();
 					}
 				}
 			}
-			logger.debug("***** Total number of image URL scanned :" + imgTtlCnt + "******");
-			logger.debug("***** Image URL failed scan :" + imgFailCnt + "******");
-			logger.debug("***** Image URL passed scan :" + imgPassCnt + "******");
+			logger.debug("Total number of image URL scanned :" + imgTtlCnt);
+			logger.debug("Image URL failed scan :" + imgFailCnt);
+			logger.debug("Image URL passed scan :" + imgPassCnt);
 		} catch (IllegalArgumentException | FeedException | IOException e) {
 			logger.error("Error occurred while validating image URL: " + e.getMessage());
 			return false;
 		}
 		return true;
 	}
+	
+	/**
+	 * Makes HTTP connection to the image URL to check for the validity
+	 * @param element
+	 * @param entry
+	 * @return CountStat
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unchecked")
+	private static CountStat connectToURL(Element element,SyndEntry entry) throws IOException{
+		CountStat countStat = new CountStat();
+		for (Element childElem : (List<Element>) element.getChildren()) {
+			if (childElem != null && childElem.getContentSize() > 0
+					&& !childElem.getText().equals("image")) {
+				String url = childElem.getContent(0).getValue();								
+				if(url!=null && !url.isEmpty()) {
+					URL obj = new URL(url);
+					HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+					con.setRequestMethod("GET");
+					int responseCode = con.getResponseCode();
+					int imgTtlCnt = countStat.getImgTtlCnt();
+					imgTtlCnt ++;
+					countStat.setImgTtlCnt(imgTtlCnt);
+					if (responseCode == 404) {
+						logger.debug(entry.getTitle() + " content url invalid:: " + url);
+						int imgFailCnt = countStat.getImgFailCnt();
+						imgFailCnt++;
+						countStat.setImgFailCnt(imgFailCnt);
+					} else {
+						int imgPassCnt = countStat.getImgPassCnt();
+						imgPassCnt++;
+						countStat.setImgPassCnt(imgPassCnt);					
+					}
+				}
+			}
+		}
+		return countStat;
+	}
 
+	/**
+	 * Parses the XML and checks for the validity
+	 * @param rssFeedUrl
+	 * @return boolean
+	 */
 	private boolean validateXML(String rssFeedUrl) {
 		try {
 
 			Reader reader = new InputStreamReader(new URL(rssFeedUrl).openStream(), "UTF-8");
 			InputSource xmlSource = new InputSource(reader);
 			xmlSource.setEncoding("UTF-8");
-
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 			docBuilder.parse(xmlSource);
-			logger.debug("***** XML validation success ******");
+			logger.debug("XML validated successfully !!");
 		} catch (ParserConfigurationException | SAXException | IOException e) {
 			logger.error("Error occurred while validating XML: " + e.getMessage());
 			return false;
@@ -109,6 +149,12 @@ public class RssFeedValidateTool {
 		return true;
 	}
 
+	/**
+	 * Verifies whether the Feed contains valid WCM elements
+	 * @param rssFeedUrl
+	 * @return boolean
+	 */
+	@SuppressWarnings("unchecked")
 	private boolean validateXMLElements(String rssFeedUrl) {
 		String[] feedElmtArr = resourceBundle.getString("feed.element.entries").split(",");
 		URL feedUrl;
@@ -122,17 +168,18 @@ public class RssFeedValidateTool {
 					List<Element> foreignElementList = (List<Element>) entry.getForeignMarkup();
 					for(Element element : foreignElementList) {
 						if(!ArrayUtils.contains(feedElmtArr, element.getQualifiedName())) {
+							logger.debug("Element not found::" +element.getQualifiedName());
 							return false;
 						}
 					}
 				}
-				logger.debug("***** XML Elements validation success ******");
+				logger.debug("XML Elements validated Successfully !!");
 			}else{
 				logger.debug("There are no XML elements to validate against..");
 			}
 			
 		} catch (IllegalArgumentException | FeedException | IOException e) {
-			logger.error("Error occurred while validating the XML elements");
+			logger.error("Error occurred while validating the XML elements" +e.getMessage());
 			return false;
 		}
 		return true;
